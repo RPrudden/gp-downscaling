5df376fb97facb7151494eeb9dabd26101134046
RPrudden <rachel.prudden@informaticslab.co.uk>
2019-02-01 13:16:33 +0000

    loglik

--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ import numpy as np
 import autograd.numpy as np
 from autograd import grad
 
-def compute_cov(l, d=1, sdim=None, X=None):
-    ker1 = gp.kern.Matern52(1, lengthscale=l)
+def compute_cov(l, d=1, sdim=None, X=None, ker=gp.kern.Matern52):
+    ker1 = ker(d, lengthscale=l)
     if not X:
         if d == 1:
             xx = np.mgrid[0:sdim]
@@ -16,6 +16,15 @@ def compute_cov(l, d=1, sdim=None, X=None):
             raise ValueError("Dimension > 2 not supported")
     C = ker1.K(X)
     return C
+
+
+def negloglik(l, l_A, ys, d=1, sdim=None, X=None):
+    C = compute_cov(l, d, sdim, X)
+    A = compute_cov(l_A, d, sdim, X)
+    ACA = np.linalg.multi_dot([A, C, A])
+
+    return -np.sum([sp.stats.multivariate_normal.logpdf(y.flatten(), mean=None, cov=ACA, 
+                                                        allow_singular=True) for y in ys])
 
 
 def logistic(x, k=1, L=1, x0=0):
@@ -51,9 +60,43 @@ def laplace_approximation(K, y):
         a2 = np.linalg.solve(np.dot(W2, L.T), a3)
         a = b - a2
         f = np.dot(K, a)
-        
+
         obj = -0.5 * np.dot(a.T, f) + log_p(y, f)
         converged = (obj < 0.001)
 
-    log_q = -0.5 * np.dot(a.T, f) + log_p(y, f) + np.trace(np.log(L))
+    log_q = -0.5 * np.dot(a.T, f) + log_p(y, f) + np.sum(np.log(np.diag(L)))
     return (f, log_q)
+
+
+def RBF(l, v=1):
+    def kern(x, x_):
+        return v**2 * np.exp((-(x-x_).T(x-x)) / (2 * l**2))
+    return kern
+
+
+def dRBF_dl(l, v=1):
+    def kern(x, x_):
+        return v**2 * np.exp((-(x-x_).T(x-x)) / (2 * l**2)) * (-(x-x_).T(x-x)) / l**3
+    return kern
+
+
+def loglik_la(X, y, l):
+    K = compute_cov(l, X, ker=RBF)
+    f, a = laplace_approximation(K, y)
+    W = -d2_log_p_df2(y, f)
+    W2 = sp.linalg.sqrtm(W)
+    B = np.eye(K.shape[0]) + np.linalg.multi_dot([W2, K, W2])
+    L = np.linalg.cholesky(B)
+    z = -0.5 * np.dot(a.T, f) + log_p(y, f) + np.sum(np.log(np.diag(L)))
+    R1 = np.linalg.solve(L, W2)
+    R = np.linalg.solve(np.dot(W2, L), R1)
+    C = np.linalg.solve(L, np.dot(W2, K))
+    s2 = -0.5 * np.diag(np.diag(K) - np.diag(np.dot(C.T, C))) * d3_log_p_df3(y, f)
+
+    C = compute_cov(l, X, ker=dRBF_dl)
+    s1 = 0.5 * np.linalg.multi_dot([a.T, C, a]) - 0.5 * np.trace(np.dot(R, C))
+    b = np.dot(C, d_log_p_df)
+    s3 = b - np.linalg.multi_dot([K, R, b])
+    dz_dl = s1 + np.dot(s2.T, s3)
+
+    return (z, dz_dl)
